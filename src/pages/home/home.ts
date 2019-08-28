ce42891ffea579f2f3c5356b16a7f6e38b290a36
unknown <acmim0809@gmail.com>
2019-08-28 15:06:35 -0300

    primeiro commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,64 @@
+import { Component, ViewChild,ElementRef } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+
+
+declare var google: any;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  start = 'chicago, il';
+  end = 'chicago, il';
+  directionsService = new google.maps.DirectionsService;
+  directionsDisplay = new google.maps.DirectionsRenderer;
+
+  constructor(
+    public navCtrl: NavController,
+    private platform: Platform
+  ) {
+    platform.ready().then(() => {
+      this.initMap();
+    })
+  }
+
+  initMap(){
+    var happycode={
+      lat: -3.7319,
+      lng: -38.5267
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, {
+      zoom: 16,
+      center: happycode,
+      disableDefaultUI: true
+    });
+
+    this.directionsDisplay.setMap(this.map);
+
+    var marker = new google.maps.Marker({
+      position: happycode,
+      map: this.map,
+      title: 'My Position!'
+    });
+  }
+  
+  calculateAndDisplayRoute(){
+    this.directionsService.route({
+      origin: this.start,
+      destination: this.end,
+      travelModel: 'DRIVING'
+    }, (response, status)=>{
+      if (status == 'OK'){
+        this.directionsDisplay.setDirections(response);
+      } 
+      else {
+        window.alert('Directions request failed due to' + status);
+      }
+    });
+  }
+}
